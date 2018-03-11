edd08eb012705ea725ceb4198259ec03b114a279
Karyna <36641408+KarynaB@users.noreply.github.com>
2018-03-11 22:00:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,42 @@
 class SmartCalculator {
   constructor(initialValue) {
     // your implementation
+    this.initialValue = initialValue;
+    return this.initialValue;
+   
   }
 
   add(number) {
     // your implementation
+    this.initialValue+=number;
+    return this.initialValue;
   }
   
   subtract(number) {
     // your implementation
+    this.initialValue-=number;
+    return this;
   }
 
   multiply(number) {
     // your implementation
+    this.initialValue*=number;
+    return this;
   }
 
   devide(number) {
     // your implementation
+    this.initialValue/=number;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    // your implementation 
+    this.initialValue=Math.pow(this.initialValue,number);
+    return this;
   }
+  
+
 }
 
 module.exports = SmartCalculator;
